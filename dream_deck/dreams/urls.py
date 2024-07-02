babf1deeac988e4fb8b031920cb064f388441a47
Zafer Arslan <zaferarslaan@gmail.com>
2024-07-02 16:11:24 +0300

    organize files

--- dream_deck/dreams/urls.py
+++ dream_deck/dreams/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from . import views
 from .views import SignUpView, LoginView
 
@@ -11,4 +13,9 @@ urlpatterns = [
     # Add any additional custom routes here
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    
+    # Simple JWT token URLs
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
